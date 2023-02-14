ba83c1a414476703b4697596d7e616bb49c40faf
Nathanaël <nathanaelkaaa@gmail.com>
2023-02-14 15:38:19 +0100

    authentification form

---
+++ login.php
@@ -0,0 +1,57 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<?php
+//require('config.php');
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'registration');
+
+// Connexion à la base de données MySQL
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Vérifier la connexion
+if($conn === false){
+    die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
+}
+
+session_start();
+if (isset($_POST['username'])){
+	$username = stripslashes($_REQUEST['username']);
+	//$username = mysqli_real_escape_string($conn, $username);
+	$password = stripslashes($_REQUEST['password']);
+    $hash = hash('sha256', $password);
+	//$password = mysqli_real_escape_string($conn, $password);
+    $query = "SELECT * FROM `users` WHERE username='$username' and password='".hash('sha256', $password)."'";
+
+    echo (json_encode($hash));
+    echo (json_encode($conn));
+    echo (json_encode($query));
+	$result = mysqli_query($conn,$query) or die(mysqli_error());
+	$rows = mysqli_num_rows($result);
+	if($rows==1){
+	    $_SESSION['username'] = $username;
+	    header("Location: index.php");
+	}else{
+		$message = "Le nom d'utilisateur ou le mot de passe est incorrect.";
+	}
+}
+?>
+<form class="box" action="" method="post" name="login">
+<h1 class="box-logo box-title"><a href="https://waytolearnx.com/">WayToLearnX.com</a></h1>
+<h1 class="box-title">Connexion</h1>
+<input type="text" class="box-input" name="username" placeholder="Nom d'utilisateur">
+<input type="password" class="box-input" name="password" placeholder="Mot de passe">
+<input type="submit" value="Connexion " name="submit" class="box-button">
+<p class="box-register">Vous êtes nouveau ici? <a href="register.php">S'inscrire</a></p>
+<?php if (! empty($message)) { ?>
+    <p class="errorMessage"><?php echo $message; ?></p>
+<?php } ?>
+</form>
+</body>
+</html>
