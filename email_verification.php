7a3eeb7b458286f2d6921207f1f9f66609316e3a
Nathanaël <nathanaelkaaa@gmail.com>
2023-02-15 13:43:08 +0100

    Double authentification

---
+++ email_verification.php
@@ -0,0 +1,79 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+//require('config.php');
+
+require 'vendor/autoload.php';
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'registration');
+
+// Connexion à la base de données MySQL
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+
+// Vérifier la connexion
+if($conn === false){
+    die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
+}
+
+session_start();
+
+
+// Vérifiez si l'utilisateur est connecté, sinon redirigez-le vers la page de connexion
+if(!isset($_SESSION["username"])){
+
+    header("Location: login.php");
+    exit();
+}
+$username = $_SESSION["username"];
+
+
+if (isset($_POST["verify_email"]))
+{
+    $query = "SELECT * FROM `users` WHERE username='$username' ";
+    $result = mysqli_query($conn,$query) or die(mysqli_error());
+    $user = mysqli_fetch_row($result);
+
+    $email = $_POST["email"];
+    $verification_input = $_POST["verification_input"];
+
+    // mark email as verified
+    //$query = "UPDATE users SET email_verified_at = NOW() WHERE email = '" . $email . "' AND verification_code = '" . $verification_code . "'";
+    $result  = mysqli_query($conn, $query);
+
+
+    if ($user[4] != $verification_input)
+    {
+        header("Location: email_verification.php");
+        exit();
+    }
+
+    header("Location: index.php");
+    exit();
+}
+?>
+<form class="box" action="" method="post" name="login">
+<h1 class="box-title">Connexion</h1>
+
+    <form method="POST">
+        <input type="hidden" name="email" value="<?php echo $_GET['email']; ?>" required>
+        <input type="text" name="verification_input" placeholder="Enter verification code" required />
+
+        <input type="submit" name="verify_email" value="Verify Email">
+    </form>
+<?php if (! empty($message)) { ?>
+    <p class="errorMessage"><?php echo $message; ?></p>
+<?php } ?>
+</form>
+</body>
+</html>
--- login.php
+++ login.php
@@ -5,7 +5,13 @@
 </head>
 <body>
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 //require('config.php');
+
+require 'vendor/autoload.php';
+
 
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
@@ -14,12 +20,13 @@ define('DB_NAME', 'registration');
 
 // Connexion à la base de données MySQL
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
+$mail = new PHPMailer(true);
 // Vérifier la connexion
+
 if($conn === false){
     die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
 }
-
+echo json_encode($_POST['username']);
 session_start();
 if (isset($_POST['username'])){
 	$username = stripslashes($_REQUEST['username']);
@@ -29,29 +36,78 @@ if (isset($_POST['username'])){
 	//$password = mysqli_real_escape_string($conn, $password);
     $query = "SELECT * FROM `users` WHERE username='$username' and password='".hash('sha256', $password)."'";
 
-    echo (json_encode($hash));
-    echo (json_encode($conn));
-    echo (json_encode($query));
+
 	$result = mysqli_query($conn,$query) or die(mysqli_error());
-	$rows = mysqli_num_rows($result);
-	if($rows==1){
+    $row = mysqli_num_rows($result);
+    $user = mysqli_fetch_row($result);
+	if($row==1){
+
+        $username = $user[1];
+        $email = $user[2];
+        $hash = $user[3];
+
+        try {
+            //Enable verbose debug output
+            $mail->SMTPDebug = 0;//SMTP::DEBUG_SERVER;
+
+            //Send using SMTP
+            $mail->isSMTP();
+
+            //Set the SMTP server to send through
+            $mail->Host = 'smtp.gmail.com';
+
+            //Enable SMTP authentication
+            $mail->SMTPAuth = true;
+
+            //SMTP username
+            $mail->Username = 'nathanaelkaaa@gmail.com';
+
+            //SMTP password
+            $mail->Password = 'wyjjnvrwtpjbzlsk';
+
+            //Enable TLS encryption;
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;//PHPMailer::ENCRYPTION_STARTTLS //ssl //PHPMailer::ENCRYPTION_SMTPS
+
+            //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+            $mail->Port = 587; //587
+
+            //Recipients
+            $mail->setFrom('nathanaelkaaa@gmail.com');
+
+            //Add a recipient
+            $mail->addAddress($email, $username);
+
+            //Set email format to HTML
+            $mail->isHTML(true);
+
+            $verification_code = substr(number_format(time() * rand(), 0, '', ''), 0, 6);
+
+            $mail->Subject = 'Email verification';
+            $mail->Body = '<p>Your verification code is: <b style="font-size: 30px;">' . $verification_code . '</b></p>';
+
+            $mail->send();
+
+            $date = date("Y-m-d h:i:sa");
+            $query = "UPDATE users SET verification_code='$verification_code' , email_verified_at=NOW() WHERE username='$username'";
+            mysqli_query($conn, $query);
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+
+
 	    $_SESSION['username'] = $username;
-	    header("Location: index.php");
+	    header("Location: email_verification.php");
 	}else{
 		$message = "Le nom d'utilisateur ou le mot de passe est incorrect.";
 	}
 }
 ?>
 <form class="box" action="" method="post" name="login">
-<h1 class="box-logo box-title"><a href="https://waytolearnx.com/">WayToLearnX.com</a></h1>
 <h1 class="box-title">Connexion</h1>
 <input type="text" class="box-input" name="username" placeholder="Nom d'utilisateur">
 <input type="password" class="box-input" name="password" placeholder="Mot de passe">
 <input type="submit" value="Connexion " name="submit" class="box-button">
-<p class="box-register">Vous êtes nouveau ici? <a href="register.php">S'inscrire</a></p>
-<?php if (! empty($message)) { ?>
-    <p class="errorMessage"><?php echo $message; ?></p>
-<?php } ?>
+
 </form>
 </body>
 </html>
